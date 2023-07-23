d9053866fb414001cc6b969c6716e6b06a13b71a
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-07-23 11:44:50 +0330

    Install font

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// Supports weights 100-900
+import "@fontsource-variable/kumbh-sans";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

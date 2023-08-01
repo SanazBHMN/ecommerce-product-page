b55a695c2e2922f4c81eef21450feb5e13cd2364
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-08-01 12:05:46 +0330

    Create a file for hard coded content

---
+++ src/helpers/Content.js
@@ -0,0 +1,12 @@
+export const content = {
+  product: {
+    title: "Fall Limited Edition Sneakers",
+    description:
+      "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+    price: {
+      original: 250.0,
+      discounted: 125.0,
+      discountPercentage: 50,
+    },
+  },
+};

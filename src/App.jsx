7fae6a2aa58e4335d108a7fc804b47416d7e6dfb
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-08-31 21:51:21 +0330

    Make the layout responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
 import MainContent from "./components/MainContent";
 import { content } from "./helpers/Content";
+import { Box, Stack } from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,20 +23,26 @@ function App() {
     },
   } = content;
   return (
-    <>
+    <Box sx={{ maxWidth: { md: "80%" }, margin: { md: "0 auto" } }}>
       <Navbar count={count} title={title} discounted={discounted} />
-      <Carousel />
-      <MainContent
-        count={count}
-        increaseCount={increaseCount}
-        decreaseCount={decreaseCount}
-        title={title}
-        description={description}
-        original={original}
-        discounted={discounted}
-        discountPercentage={discountPercentage}
-      />
-    </>
+      <Stack
+        direction={{ sm: "column", md: "row" }}
+        alignItems="stretch"
+        height="100vh"
+      >
+        <Carousel />
+        <MainContent
+          count={count}
+          increaseCount={increaseCount}
+          decreaseCount={decreaseCount}
+          title={title}
+          description={description}
+          original={original}
+          discounted={discounted}
+          discountPercentage={discountPercentage}
+        />
+      </Stack>
+    </Box>
   );
 }
 

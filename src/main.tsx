8fb1c50d44cf0e507c2d48e782960d369f568785
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-04-27 11:30:42 +0330

    Set up react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,50 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.tsx";
+import Root from "./pages/Root.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import CollectionsPage from "./pages/CollectionsPage.tsx";
+import WomenPage from "./pages/WomenPage.tsx";
+import MenPage from "./pages/MenPage.tsx";
+import AboutPage from "./pages/AboutPage.tsx";
+import ContactPage from "./pages/ContactPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/collections",
+        element: <CollectionsPage />,
+      },
+      {
+        path: "/women",
+        element: <WomenPage />,
+      },
+      {
+        path: "/men",
+        element: <MenPage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );

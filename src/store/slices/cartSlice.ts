370f945ce44339e09f7c832a4db8dc04be79eb06
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-04-30 11:51:12 +0330

    Implement cartSlice logic

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -2,25 +2,43 @@
 // - Items added to cart
 // - Items removed from cart
 // - clearCart action
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CartItem } from "../../types/types";
+
+interface CartState {
+  data: CartItem[];
+}
+
+const initialState: CartState = {
+  data: [],
+};
 
 const cartSlice = createSlice({
   name: "cart",
-  initialState: {
-    data: [],
-  },
+  initialState,
   reducers: {
-    addItem(state, action) {
+    addItem(state, action: PayloadAction<CartItem>) {
       // Assumption:
       // action.payload === { name: 'ab', price: 1, totalCost: 1}
+      const existingItem = state.data.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingItem) {
+        existingItem.quantity += action.payload.quantity;
+        existingItem.totalCost += action.payload.totalCost;
+      } else {
+        state.data.push(action.payload);
+      }
     },
-    removeItem(state, action) {
+    removeItem(state, action: PayloadAction<string>) {
       // Assumption:
       // action.payload === th id of the item we want to remove
+      state.data = state.data.filter((item) => item.id !== action.payload);
     },
     clearCart(state, action) {
       // Assumption:
       // reset the state
+      state.data = [];
     },
   },
 });

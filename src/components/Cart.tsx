9185f9fa3c30ce88a8f0b5e1a0db80e0d0f8efcc
Sanaz Bahmani <sanazbahmani35@gmail.com>
2025-05-17 15:26:11 +0330

    Display the relevant text when the cart is empty

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,15 +1,20 @@
 import { Unstable_Popup as BasePopup } from "@mui/base/Unstable_Popup";
 import { styled } from "@mui/system";
+// imports from MUI
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Divider from "@mui/material/Divider";
 // import assets
 import { CartItem } from "../types/types";
+import CartList from "./CartList";
 
 const PopupBody = styled("div")(
   ({ theme }) => `
-    width: 90vw;
+    width: 350px;
     padding: 12px 16px;
     margin: 8px;
     border-radius: 8px;
-    border: 1px solid red;
     background-color: white;
     z-index: 1;
     `
@@ -25,12 +30,36 @@ interface CartProps {
 function Cart({ id, open, anchor, cartItems }: CartProps) {
   return (
     <BasePopup id={id} open={open} anchor={anchor}>
-      <PopupBody>
-        {cartItems.length === 0
-          ? "Your cart is empty"
-          : cartItems.map(({ id, name, price, quantity, totalCost }) => (
-              <>{quantity}</>
-            ))}
+      <PopupBody sx={{ padding: "0", minHeight: "300px" }}>
+        {cartItems.length === 0 ? (
+          <>
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{
+                padding: "8px 16px",
+              }}
+            >
+              Cart
+            </Typography>
+            <Divider />
+            <CardContent
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "300px",
+              }}
+            >
+              <Typography>Your cart is empty</Typography>
+            </CardContent>
+          </>
+        ) : (
+          // cartItems.map(({ id, name, price, quantity, totalCost }) => (
+          //   <>{quantity}</>
+          // ))
+          <CartList />
+        )}
       </PopupBody>
     </BasePopup>
   );
--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -1,10 +1,10 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+// imports from MUI
 
 function CartList() {
   const cartItems = useSelector((state: RootState) => state.cart.data);
 
-  console.log(cartItems);
   return <div>{cartItems.map((item) => item.quantity)}</div>;
 }
 
